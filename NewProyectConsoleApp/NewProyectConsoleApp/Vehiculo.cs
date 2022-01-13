03608cbf37f48e1802d1ef81811c429a0f91b86c
adriangaray396 <adriangaray396@gmail.com>
2022-01-13 16:33:43 -0300

    Se agrego la clase Vehiculo.

---
+++ NewProyectConsoleApp/NewProyectConsoleApp/Vehiculo.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NewProyectConsoleApp
+{
+    class Vehiculo
+    {
+        public decimal VelocidadMaxima { get; set; }
+        public Vehiculo(decimal velocidadMaxima)
+        {
+            VelocidadMaxima = velocidadMaxima;
+        }
+    }
+}
